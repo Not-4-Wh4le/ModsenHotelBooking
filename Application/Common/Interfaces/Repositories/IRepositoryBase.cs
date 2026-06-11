bf505db9352398b5a4be25fb0c5d92f807100f68
notawhale <notawh4le@gmail.com>
2026-06-11 04:50:26 +0300

    feat(application): add repository and unit of work interfaces

---
+++ Application/Common/Interfaces/Repositories/IRepositoryBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Common.Interfaces.Repositories
+{
+    public interface IRepositoryBase<T> where T : class
+    {
+        Task<T?> GetById(Guid id, CancellationToken cancellationToken);
+        Task AddAsync(T entity, CancellationToken cancellationToken);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
